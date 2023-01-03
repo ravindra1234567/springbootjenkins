41132d2331479dba8666ad24d06876b88d5207d9
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2023-01-03 17:13:08 +0530

    change pom.xml

--- src/test/java/com/pipeline/pipeline/PipelineApplicationTests.java
+++ src/test/java/com/pipeline/pipeline/PipelineApplicationTests.java
@@ -1,13 +1,1 @@
-package com.pipeline.pipeline;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PipelineApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
